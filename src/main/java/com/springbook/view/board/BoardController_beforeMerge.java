6dfd55ec9e59cf209ef42eab1c85bc2ea8384e95
Administrator <Administrator@WIN-BANUSM3LV7K>
2018-02-23 14:24:59 +0900

    Layer통합, service Autowired처리

--- src/main/java/com/springbook/view/board/BoardController_beforeMerge.java
+++ src/main/java/com/springbook/view/board/BoardController_beforeMerge.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,17 +17,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
 //Controller클래스의 메소드들을 하나로 통합 후
 //Component 어노테이션을 주석처리하여 원래의 POJO 클래스로 변환처리함.
-@Controller
-@SessionAttributes("board")//board라는 이름으로 객체를 session에 저장하는 Annotation
-public class BoardController {
-	@Autowired
-	BoardService boardService;
-	
+///@Controller
+//@SessionAttributes("board")//board라는 이름으로 객체를 session에 저장하는 Annotation
+public class BoardController_beforeMerge {
 	//검색 조건 목록 설정
 	@ModelAttribute("conditionMap")
 	public Map<String, String> searchConditionMap(){
@@ -41,13 +36,13 @@ public class BoardController {
 	//글등록
 	@RequestMapping("/insertBoard.do")
 	 public String insertBoard(HttpServletRequest request) {
-	  System.out.println("글 등록 폼");
+	  System.out.println("글 등록 처리");
 	 
 	HttpSession session = request.getSession();
 		 String id = (String)session.getAttribute("id");
 		 String writer = request.getParameter("title");
 		 String content = request.getParameter("content");
-
+		 //ModelAndView mav = new ModelAndView();
 		 String view = "";
 	        if(id==null|"".equals(id))
 	       	 	view = "redirect:login.do";
@@ -58,7 +53,8 @@ public class BoardController {
 	//글 등록 처리
 	@RequestMapping(value="/insertBoardProc.do")
 	public String insertBoardProc( HttpServletRequest request,
-									BoardVO vo) {
+									BoardVO vo,
+									BoardDAO dao) {
 		HttpSession session = request.getSession();
 		 String id = (String)session.getAttribute("id");
 		 String view ="";
@@ -68,7 +64,7 @@ public class BoardController {
 	        	try {
 	        	System.out.println("글 등록 처리");
 		    	//2.DB연동 처리
-	      	   boardService.insertBoard(vo);//boardService 등록
+		    	dao.insertBoard(vo);
 		    	view = "redirect:getBoardList.do";
 	        	}catch(Exception e) {System.out.println(e.getMessage());}
 
@@ -78,7 +74,8 @@ public class BoardController {
 	//글 수정
 	@RequestMapping("/updateBoard.do")
 	public String updateBoard(HttpServletRequest request,
-			                  @ModelAttribute("board") BoardVO vo) {
+			                  @ModelAttribute("board") BoardVO vo,
+			                  BoardDAO dao) {
 		HttpSession session = request.getSession();
 		 String id = (String)session.getAttribute("id");
 		 String view ="";
@@ -88,7 +85,7 @@ public class BoardController {
 	        	try {
 	        		System.out.println("글 수정 처리");
 		    	//2. DB연동처리
-		    	boardService.updateBoard(vo);
+		    	dao.updateBoard(vo);
 		    	//3. 화면 이동
 		    	view = "redirect:getBoardList.do";
 	        	}catch(Exception e) {System.out.println(e.getMessage());}
@@ -109,8 +106,8 @@ public class BoardController {
 	        	try {
 	        		System.out.println("글 삭제 처리");
 	    	    	String seq = request.getParameter("seq");
-	    	    	
-	    	    	int result = boardService.deleteBoard(Integer.parseInt(seq));
+	    	    	BoardDAO dao = new BoardDAO();
+	    	    	int result = dao.deleteBoard(Integer.parseInt(seq));
 	    	    	if(result>0)
 	    	    		view = "redirect:getBoardList.do";
 	    	    	else {
@@ -130,6 +127,7 @@ public class BoardController {
 	@RequestMapping("/getBoard.do")
 	public String getBoard(HttpServletRequest request,
 							BoardVO vo,
+							BoardDAO dao,
 							Model model
 							) {	
 		ModelAndView mav = new ModelAndView();
@@ -140,7 +138,7 @@ public class BoardController {
 			view = "redirect:login.do";
 		else {
 			System.out.println("글 상세 처리");
-			vo = boardService.getBoard(vo.getSeq());// 객체 저장,BoardServiceImple의 메소드 사용
+			vo = dao.getBoard(vo.getSeq());// 객체 저장
 			model.addAttribute("board", vo);
 
 			view = "getBoard.jsp";
@@ -150,7 +148,10 @@ public class BoardController {
 	
 	//글 목록 검색
 	@RequestMapping(value="/getBoardList.do")
-	public String getBoardList(HttpServletRequest request,Model model,			                 
+	public String getBoardList(HttpServletRequest request,Model model,
+			                   //request.getParameter("searchCondition")와 비슷한 기능
+								/*@RequestParam(value="searchCondition",defaultValue="TITLE",required=false) String condition,
+								@RequestParam(value="searchKeyword",defaultValue="",required=false) String keyword*/
 			                    BoardVO vo ) {
 		System.out.println("글 목록 검색 처리");
 		HttpSession session = request.getSession();
@@ -167,7 +168,9 @@ public class BoardController {
 	    	   vo.setSearchCondition("TITLE");
 	       if(vo.getSearchKeyword()==null)
 	    	   vo.setSearchKeyword("");
-   		List<BoardVO> list = boardService.getBoardList(vo);//businessLayer쪽의 메소드 사용
+  /* 	BoardVO vo = new BoardVO();*/
+   /*	List<BoardVO> list = dao.getBoardList(vo);*/
+   		List<BoardVO> list = dao.getBoardList(vo.getSearchCondition(),vo.getSearchKeyword());
   
    	model.addAttribute("boardList", list);
    	view = "getBoardList.jsp";
